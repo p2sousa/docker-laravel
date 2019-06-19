9682c581da0ff5552fd687cf75f58e6805382e51
Pablo Sousa <pablosousa@startstudio.com.br>
2019-06-19 20:06:14 -0300

    adicionado chmod 755 storage no entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+echo "Alterando permissao da pasta storage...."
+chmod -R 775 storage/
+
+echo "Instalando dependencias php...."
 composer install
+
+echo "Instalando dependencias node...."
 npm install
+
+echo "rodando migrations...."
 php artisan key:generate
 php artisan migrate
 
